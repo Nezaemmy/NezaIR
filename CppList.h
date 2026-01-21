d1d09a609a75455bd43240643346c079f2f2b6e7
Nezaemmy <38810350+Nezaemmy@users.noreply.github.com>
2026-01-21 14:48:16 +0100

    Update CppList.h

--- CppList.h
+++ CppList.h
@@ -1,27 +1,48 @@
-//Modified for multiple IR
 #ifndef CPPLIST_H
 #define CPPLIST_H
 
-#if !defined(IR_MAXCOUNTIR)
-#define IR_MAXCOUNTIR 20
-#endif
-
 #include "IRremoteInt.h"
 
-class CppList
-{
+#ifndef IR_MAXCOUNTIR
+#define IR_MAXCOUNTIR 8
+#endif
+
+class CppList {
 public:
-  CppList();
-  void Add(irparams_struct *item);
-  int GetCount();
-  irparams_struct* GetItem(int index);
+    CppList() : _count(0) {}
+
+    inline void Add(irparams_struct *item) {
+        if (item == nullptr) return;
+        if (Exists(item)) return;
+        if (_count >= IR_MAXCOUNTIR) return;
+        _arr[_count++] = item;
+    }
+
+    inline int GetCount() const {
+        return _count;
+    }
+
+    inline irparams_struct* GetItem(int index) const {
+        if (index < 0) return nullptr;
+        if (index >= _count) return nullptr;
+        return _arr[index];
+    }
+
 private:
-  bool Exists(void *item);
-  int GetIndex(void *item);
-  int _count, _maxCount;
-  irparams_struct* _arr[IR_MAXCOUNTIR];
-  void PrepareArray(int nextSize);
+    inline bool Exists(const void *item) const {
+        return GetIndex(item) != -1;
+    }
+
+    inline int GetIndex(const void *item) const {
+        for (int i = 0; i < _count; ++i) {
+            if (item == _arr[i]) return i;
+        }
+        return -1;
+    }
+
+    int _count;
+    irparams_struct* _arr[IR_MAXCOUNTIR];
 };
 
-#endif // #ifndef CPPLIST_H
+#endif // CPPLIST_H
 #pragma once
